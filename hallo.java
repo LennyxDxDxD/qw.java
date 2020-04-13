1650dff74cfd74cd09eb2c45708ba2bd33e56368
Lennart Horney <lehor104@uni-duesseldorf.de>
2020-04-13 16:18:22 +0200

    test

---
+++ hallo.java
@@ -0,0 +1,9 @@
+public class hallo
+{
+ 
+       public static void main (String[] args)
+       {
+             // Ausgabe Hello World!
+             System.out.println("Hello World!");
+       }
+}
